560018b5dfc7bb341b3403ba1726345ddd7bf367
Victor Raul Gutierrez Sanabria <gutierrezsanabria@gmail.com>
2024-02-17 16:26:06 -0500

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import './Footer.css'
+
+
+export function Footer () {
+  // const { filters } = useFilters()
+
+  return (
+    <footer className='footer'>
+
+      <h4>Prueba técnica 3 nivel de React de Funval ⚛️ － <span>@gutierrez</span></h4>
+      
+      <h5>Shopping Cart con useContext & useReducer ✈    </h5>
+    </footer>
+  )
+}
---
+++ src/context/filters.jsx
@@ -0,0 +1,27 @@
+import { createContext, useState } from 'react'
+
+
+export const FiltersContext = createContext()
+
+
+// Este es el que nos provee de acceso al contexto
+
+
+export function FiltersProvider ({ children }) {
+  const [filters, setFilters] = useState({
+    category: 'all',
+
+    minPrice: 250
+  })
+
+  return (
+    
+    <FiltersContext.Provider value={{
+      filters,
+      setFilters
+    }}
+    >
+      {children}
+    </FiltersContext.Provider>
+  )
+}
